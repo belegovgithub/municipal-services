9c3ee3de65bd6b188519ced4607573f786aa64bb
Rajanihn <rajnihn@gmail.com>
2020-07-09 17:24:43 +0530

    Multiple application numbers for renewal application--resolved

--- tl-services/src/main/java/org/egov/tl/service/TradeLicenseService.java
+++ tl-services/src/main/java/org/egov/tl/service/TradeLicenseService.java
@@ -256,7 +256,7 @@ public class TradeLicenseService {
         TradeLicense licence = tradeLicenseRequest.getLicenses().get(0);
         TradeLicense.ApplicationTypeEnum applicationType = licence.getApplicationType();
         List<TradeLicense> licenceResponse = null;
-        if(applicationType != null && (applicationType).toString().equals(TLConstants.APPLICATION_TYPE_RENEWAL )&& licence.getAction().equalsIgnoreCase(TLConstants.TL_ACTION_INITIATE)){
+        if(applicationType != null && (applicationType).toString().equals(TLConstants.APPLICATION_TYPE_RENEWAL )&& licence.getAction().equalsIgnoreCase(TLConstants.TL_ACTION_INITIATE) && !(licence.getStatus().equalsIgnoreCase(TLConstants.TL_STATUS_INITIATED))){
             List<TradeLicense> createResponse = create(tradeLicenseRequest, businessServicefromPath);
             licenceResponse =  createResponse;
         }
