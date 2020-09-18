e2d0c7bf47be292c7653e0e5e6b529ef87d1b762
minju <minju.sarah@gmail.com>
2020-09-18 10:04:22 +0530

    Changed Boundary to ADMIN

--- rainmaker-pgr/src/main/java/org/egov/pgr/utils/PGRConstants.java
+++ rainmaker-pgr/src/main/java/org/egov/pgr/utils/PGRConstants.java
@@ -79,7 +79,8 @@ public class PGRConstants {
 	
 	public static final String LOCATION__BOUNDARY_NAMES_JSONPATH = "$.TenantBoundary.*.boundary.*.name";
 	public static final String LOCATION__BOUNDARY_CODES_JSONPATH = "$.TenantBoundary.*.boundary.*.code";
-	public static final String LOCATION__BOUNDARY_HIERARCHYTYPE_ADMIN = "ADMIN";
+	//public static final String LOCATION__BOUNDARY_HIERARCHYTYPE_ADMIN = "ADMIN";
+	public static final String LOCATION__BOUNDARY_HIERARCHYTYPE_ADMIN = "REVENUE";
 	public static final String LOCATION__BOUNDARY_BOUNDARYTYPE_LOCALITY = "Locality";
 
 	
