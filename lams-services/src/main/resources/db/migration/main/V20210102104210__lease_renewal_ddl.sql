8ecbe838f4d4b2febb78c09c55e5df6fd650bd66
santhups52 <santhups52@gmail.com>
2021-02-02 10:16:19 +0530

    bug fix

--- lams-services/src/main/resources/db/migration/main/V20210102104210__lease_renewal_ddl.sql
+++ lams-services/src/main/resources/db/migration/main/V20210102104210__lease_renewal_ddl.sql
@@ -3,3 +3,9 @@ ALTER TABLE public.eg_lams_leaserenewal
 
 ALTER TABLE public.eg_lams_leaserenewal
     ADD COLUMN months character varying(5);
+    
+ALTER TABLE public.eg_lams_leaserenewal_audit
+    ADD COLUMN fatherorhusbandname character varying(64);
+
+ALTER TABLE public.eg_lams_leaserenewal_audit
+    ADD COLUMN months character varying(5);
