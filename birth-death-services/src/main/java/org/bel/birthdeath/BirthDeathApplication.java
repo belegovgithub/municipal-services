67580cb4aef21250644ab4a2bd8907eb651f1721
santhups52 <santhups52@gmail.com>
2021-03-05 10:54:31 +0530

    birth service my appls changes- bug fix

--- birth-death-services/src/main/java/org/bel/birthdeath/BirthDeathApplication.java
+++ birth-death-services/src/main/java/org/bel/birthdeath/BirthDeathApplication.java
@@ -1,8 +1,5 @@
 package org.bel.birthdeath;
 
-import javax.annotation.PostConstruct;
-
-import org.bel.birthdeath.birth.model.SearchCriteria;
 import org.bel.birthdeath.birth.repository.BirthRepository;
 import org.bel.birthdeath.birth.service.BirthService;
 import org.bel.birthdeath.common.services.CommonService;
@@ -17,7 +14,6 @@ import org.springframework.context.annotation.Import;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 
 @SpringBootApplication(scanBasePackages = "org.bel.birthdeath")
 @EnableAutoConfiguration
@@ -45,22 +41,4 @@ public class BirthDeathApplication {
                 .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
     }
 	
-	@PostConstruct
-	private void start() {
-		System.out.println("in");
-		SearchCriteria criteria = new SearchCriteria();
-		criteria.setTenantId("pb.testing");
-		//criteria.setMotherName("a");
-		criteria.setDateOfBirth("23-02-2021");
-		criteria.setGender(1);
-		//criteria.setHospitalId("1");
-		criteria.setRegistrationNo("2021-1");
-		//criteria.setId("1");
-		//System.out.println(new Gson().toJson(br.getBirthDtlsAll(criteria)));
-		//birthService.download(criteria);
-		//System.out.println(new Gson().toJson(commonService.search(criteria.getTenantId())));
-		//br.updateCounter("1");
-		//System.out.println(br.getBirthCertReqByConsumerCode("abcd").getBirthDtlId());
-		System.out.println(new Gson().toJson(br.searchApplications("pb.testing", "a5d89427-75a0-4251-b9d5-19c152972cec")));
-	}
 }
