ace146a6db32189e72592712f96eb72d57b2e36a
santhups52 <santhups52@gmail.com>
2021-03-23 09:56:47 +0530

    Bnd Enc,decr bug fix

--- birth-death-services/src/main/java/org/bel/birthdeath/BirthDeathApplication.java
+++ birth-death-services/src/main/java/org/bel/birthdeath/BirthDeathApplication.java
@@ -1,16 +1,24 @@
 package org.bel.birthdeath;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.bel.birthdeath.birth.repository.BirthRepository;
 import org.bel.birthdeath.birth.service.BirthService;
 import org.bel.birthdeath.common.services.CommonService;
 import org.egov.encryption.config.EncryptionConfiguration;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
@@ -18,6 +26,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication(scanBasePackages = "org.bel.birthdeath" )
 @EnableAutoConfiguration
+//@Import({TracerConfiguration.class})
 @Import({TracerConfiguration.class,EncryptionConfiguration.class})
 public class BirthDeathApplication {
 
@@ -29,6 +38,11 @@ public class BirthDeathApplication {
 	
 	@Autowired
 	BirthRepository br;
+	
+	@Value("${app.timezone}")
+    private String timeZone;
+	
+	private static final String DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BirthDeathApplication.class, args);
@@ -42,4 +56,20 @@ public class BirthDeathApplication {
                 .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
     }
 	
+	@Bean
+    public MappingJackson2HttpMessageConverter jacksonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH));
+        mapper.setTimeZone(TimeZone.getTimeZone(timeZone));
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
+	
+	@PostConstruct
+    public void initialize() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+    }
 }
